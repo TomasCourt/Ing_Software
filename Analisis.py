b7838b9fe3b1177faf03566aa545adf9ab03725f
TomasCourt <80427884+TomasCourt@users.noreply.github.com>
2022-11-23 04:05:45 -0300

    Funcionamiento correcto de la cantidad de productos vendidos y comienzo del analisis

--- Analisis.py
+++ Analisis.py
@@ -5,31 +5,39 @@ import numpy as np
 ipath = 'C:/Users/ASUS/Desktop/Informática/Ing_Software/Ventas.xlsx'
 
 df = pd.read_excel(ipath)
-print(df["codigo"].size)
+#print(df["codigo"].size)
 
 ipath2= 'C:/Users/ASUS/Desktop/Informática/Ing_Software/listaProductos.xlsx'
 
 df2 = pd.read_excel(ipath2)
-print(df2.size)
+#print(df2.size)
 
 
 Productos=pd.DataFrame()
 Productos["cantidad"]=None
 Productos["codigo"]=None
 
+#print(df["codigo"].unique())
 
 for a in df["codigo"].unique():
-    #print("Hay ",df[df["codigo"]==a].size," productos del codigo: ",a)
-    Productos=pd.concat([Productos,(pd.DataFrame({"cantidad":df[df["codigo"]==a].size,"codigo":a}, index=[0]))])
+    df3=df[df["codigo"]==a]
+    #print("Hay ",df3["codigo"].size," productos del codigo: ",a)
+    Productos=pd.concat([Productos,(pd.DataFrame({"cantidad":df3["codigo"].size,"codigo":a}, index=[0]))])
 
 #ordenamos la el dataframe segun la cantidad de veces comprado, y nos enfocamos en esos productos para mejorar el funcionamiento del stock
 #De esta forma lograr un impacto mayor
 
-by_cantidadVentas = Productos.sort_values('cantidad',ascending=False)
+by_cantidadVentas = Productos.sort_values('cantidad',ascending=False).reset_index()
 
 print(by_cantidadVentas.head(5))
 
-for a in range(0,6):    
-    df3=df2[df2["codigo"]==by_cantidadVentas["codigo"][a]] 
-    print("Aquel producto vendido ",by_cantidadVentas["cantidad"][a]," veces, es: ",df3["producto"]," y vence en: ",df3["duracion"])
+for f in range(0,6):
+    cont=0
+    for a in df2["codigo"]:
+        if (a == str(by_cantidadVentas["codigo"][f])):
+            print("Aquel producto vendido ",by_cantidadVentas["cantidad"][f]," veces, es: ",df2["producto"][cont]," y vence en: ",df2["duracion"][cont])
+        cont+=1
+
+#Con lo anterior pudimos notar que hay productos que se venden en gran cantidad y que ademas, vencen muy rapido
+
 
